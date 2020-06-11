0857282da7fc14fe57bd139158d3fe7c8c8574d9
Stylian <stylianos.chatzimanolis@dotmatics.com>
2020-06-11 15:06:24 +0300

    added basic orbit example

--- src/sprites/moon.js
+++ src/sprites/moon.js
@@ -1,13 +1,15 @@
 import Phaser from 'phaser'
 
 export default class extends Phaser.Sprite {
-  constructor ({ game, x, y, asset }) {
+
+  constructor ({ game, x, y, vx, vy, asset }) {
     super(game, x, y, asset)
     this.anchor.setTo(0.5)
+
+    this.vx = vx
+    this.vy = vy
   }
 
   update () {
-    this.x = 400
-    this.y = 200
   }
 }
--- src/states/Game.js
+++ src/states/Game.js
@@ -1,22 +1,26 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
 import Mushroom from '../sprites/Mushroom'
+import Moon from '../sprites/Moon'
 import lang from '../lang'
 
 export default class extends Phaser.State {
-  init () { }
-  preload () { }
+  init () {
+  }
+
+  preload () {
+  }
 
   create () {
-    const bannerText = lang.text('welcome')
-    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText, {
-      font: '40px Bangers',
-      fill: '#7791bf',
-      smoothed: false
-    })
-
-    banner.padding.set(10, 16)
-    banner.anchor.setTo(0.5)
+    // const bannerText = lang.text('welcome')
+    // let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText, {
+    //   font: '40px Bangers',
+    //   fill: '#7791bf',
+    //   smoothed: false
+    // })
+    //
+    // banner.padding.set(10, 16)
+    // banner.anchor.setTo(0.5)
 
     this.mushroom = new Mushroom({
       game: this.game,
@@ -25,12 +29,43 @@ export default class extends Phaser.State {
       asset: 'mushroom'
     })
 
+    this.moon = new Moon({
+      game: this.game,
+      x: 200,
+      y: 200,
+      vx: 0,
+      vy: 0.7,
+      asset: 'moon'
+    })
+
     this.game.add.existing(this.mushroom)
+    this.game.add.existing(this.moon)
   }
 
   render () {
     if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+      this.game.debug.spriteInfo(this.mushroom, 8, 8)
+      this.game.debug.spriteInfo(this.moon, 8, 8)
     }
+
+    let distX = this.mushroom.x - this.moon.x
+    let distY = this.mushroom.y - this.moon.y
+    let dist = Math.sqrt(Math.pow(distX, 2) + Math.pow(distY, 2))
+    dist = dist < 50 ? 50 : dist
+
+    let G = 200
+
+    let ang = Math.atan2(distY, distX)
+    let acc = 1 / Math.pow(dist, 2) * G
+
+    let accX = acc * Math.cos(ang)
+    let accY = acc * Math.sin(ang)
+
+    this.moon.vx += accX
+    this.moon.vy += accY
+
+    this.moon.x += this.moon.vx
+    this.moon.y += this.moon.vy
+
   }
 }
